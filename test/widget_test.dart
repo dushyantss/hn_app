938ce2b24df677b514f37a0368ab681daa50309a
Dushyant Singh Shekhawat <contact@dushyantss.com>
2019-03-10 00:39:15 +0530

    First episode done with fixed test and key.

---
+++ test/widget_test.dart
@@ -0,0 +1,24 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:hn_app/main.dart';
+
+void main() {
+  testWidgets('Clicking a tile opens it', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    expect(find.byIcon(Icons.launch), findsNothing);
+
+    await tester.tap(find.byType(ExpansionTile).first);
+    await tester.pump();
+
+    expect(find.byIcon(Icons.launch), findsOneWidget);
+  });
+}
